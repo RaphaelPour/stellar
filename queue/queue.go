59c70df378cf494c09fa2481873249d9674b185f
Raphael Pour <info@raphaelpour.de>
2022-12-12 15:21:19 +0100

    queue: add initializer

--- queue/queue.go
+++ queue/queue.go
@@ -4,6 +4,11 @@ type Queue[T any] struct {
 	items []T
 }
 
+func NewQueue[T any]() *Queue[T]{
+	q := new(Queue[T])
+	q.items = make([]T,0)
+}
+
 func (q *Queue[T]) Enqueue(item T) {
 	q.items = append(q.items, item)
 }
