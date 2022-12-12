8b8107212344098273b7d8f25bba3ca6eedfe741
Raphael Pour <info@raphaelpour.de>
2022-12-12 13:44:57 +0100

    pkg: add queue type

---
+++ queue/queue.go
@@ -0,0 +1,23 @@
+package queue
+
+type Queue[T any] struct {
+	items []T
+}
+
+func (q *Queue[T]) Enqueue(item T) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue[T]) Dequeue() T {
+	if len(q.items) == 0 {
+		panic("queue is empty")
+	}
+
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item
+}
+
+func (q Queue[T]) Count() int {
+	return len(q.items)
+}
