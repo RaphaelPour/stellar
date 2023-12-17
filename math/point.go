faf3004b29551594493b43314415db7d85a60cb1
Raphael Pour <info@raphaelpour.de>
2023-12-17 13:55:30 +0100

    math/point: add more functions

--- math/point.go
+++ math/point.go
@@ -1,11 +1,60 @@
 package math
 
-type Point[T Number] struct {
-	X, Y T
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y int
 }
 
-func (p Point[T]) Add(other Point[T]) Point[T] {
+func (p Point) Add(other Point) Point {
 	p.X += other.X
 	p.Y += other.Y
+	return p
+}
+
+func (p Point) Sub(other Point) Point {
+	p.X -= other.X
+	p.Y -= other.Y
+	return p
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("%d/%d", p.X, p.Y)
+}
+
+func (p Point) ManhattanDist(other Point) int {
+	return Abs(p.X-other.X) + Abs(p.Y-other.Y)
+}
+
+func (p Point) EuclideanDist(other Point) float64 {
+	return math.Sqrt(float64(Pow(Abs(p.X-other.X), 2) + Pow(Abs(p.Y-other.Y), 2)))
+}
+
+func (p Point) Equal(other Point) bool {
+	return p.X == other.X && p.Y == other.Y
+}
+
+func (p Point) Min(other Point) Point {
+	if p.X > other.X {
+		p.X = other.X
+	}
+
+	if p.Y > other.Y {
+		p.Y = other.Y
+	}
+	return p
+}
+
+func (p Point) Max(other Point) Point {
+	if p.X < other.X {
+		p.X = other.X
+	}
+
+	if p.Y < other.Y {
+		p.Y = other.Y
+	}
 	return p
 }
