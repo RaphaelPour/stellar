d30023eea5649ef9ee2473fc6e957569e42754d5
Raphael Pour <info@raphaelpour.de>
2024-11-15 14:47:54 +0100

    add iterator

---
+++ iter/iter.go
@@ -0,0 +1,34 @@
+package iter
+
+import (
+	"iter"
+	"slices"
+
+	"github.com/RaphaelPour/stellar/math"
+)
+
+func Pairs[S ~[]T, T any](in S, num int) []S {
+	return slices.Collect(PairsSeq(in, num))
+}
+
+func PairsSeq[S ~[]T, T any](in S, num int) iter.Seq[S] {
+	return func(yield func(S) bool) {
+		for i := 0; i < len(in); i += num {
+			if !yield(in[i:math.Min(i+num, len(in))]) {
+				return
+			}
+		}
+	}
+}
+
+func PairSeq2[S ~[]T, T any](in S, num int) iter.Seq2[int, S] {
+	return func(yield func(int, S) bool) {
+		index := 0
+		for i := 0; i < len(in); i += num {
+			if !yield(index, in[i:math.Min(i+num, len(in))]) {
+				return
+			}
+			index += 1
+		}
+	}
+}
