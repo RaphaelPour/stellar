50536fa08a55e4db33d0ac84237d6cd6d4677a2e
Raphael Pour <info@raphaelpour.de>
2024-12-03 15:45:51 +0100

    hack: add yolo suite

--- hack/hack.go
+++ hack/hack.go
@@ -15,3 +15,22 @@ func Bool[T Number](in T) bool {
 func Wormhole(in bool) int {
 	return (*(*int)(unsafe.Pointer(&in))) & int(1)
 }
+
+func Yolo[T any](value T, _ error) T {
+	return value
+}
+
+func YoloBoom[T any](value T, err error) T {
+	if err != nil {
+		panic(err.Error())
+	}
+	return value
+}
+
+func YoloDefault[T any](value T, err error) T {
+	if err == nil {
+		return value
+	}
+	var def T
+	return def
+}
